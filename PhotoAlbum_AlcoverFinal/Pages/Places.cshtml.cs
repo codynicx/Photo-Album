7601d4695fdba697e2161758fd135fb1caadd400
codynicx <alcovergero@gmail.com>
2024-01-10 09:13:23 +0800

    first commit

---
+++ PhotoAlbum_AlcoverFinal/Pages/Places.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoAlbum_AlcoverFinal.Pages
+{
+    public class PlacesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PhotoAlbum_AlcoverFinal/Pages/SelfPortrait.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoAlbum_AlcoverFinal.Pages
+{
+    public class SelfPortraitModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
